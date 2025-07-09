e2400767ab2eac7b47643a472cc5eb17c8436061
RezaAbdiRad <rezaabdi58@gmail.com>
2025-07-09 17:11:40 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+  const app = document.getElementById('app');
+  app.innerHTML = '<h1>VIM Peygiri Tracker - JS Logic Coming Soon</h1>';
+});
