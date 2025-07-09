08658d38c9b83635b7b435f2e7f245ee670b5676
RezaAbdiRad <rezaabdi58@gmail.com>
2025-07-09 17:15:25 +0330

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-// script.js
-document.addEventListener('DOMContentLoaded', function () {
-  const app = document.getElementById('app');
-  app.innerHTML = '<h1>VIM Peygiri Tracker - JS Logic Coming Soon</h1>';
-});
